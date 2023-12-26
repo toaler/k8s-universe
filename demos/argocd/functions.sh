a51889c9709997fda121a22eba04c73e3dc5261f
Brian Toal <btoal@Brians-iMac.local>
2023-12-25 22:58:32 -0800

    Fixed OSX compatibility issues

--- demos/argocd/functions.sh
+++ demos/argocd/functions.sh
@@ -1,11 +1,11 @@
- log() {
+log() {
      local timestamp
      timestamp=$(date +"%Y-%m-%d %T")
      local log_message="$timestamp [INFO] : $1"
      echo "$log_message"
- }
+}
 
- fail() {
+fail() {
    set +x
    local -r all_args=("$@")
    local -r reason=$1
@@ -27,4 +27,28 @@
    fi
 
    exit 1
- }
+}
+
+retry_with_backoff() {
+  local retries=5
+  local max_retries=5
+  local delay=1
+
+  local command_to_retry="$@"
+
+  while (( retries > 0 )); do
+    echo "Attempt: $((max_retries - retries + 1))"
+    if eval "$command_to_retry"; then
+      echo "Command executed successfully!"
+      return 0
+    else
+      echo "Command failed. Retrying in $delay seconds..."
+      sleep $delay
+      (( delay *= 2 ))  # Exponential backoff, doubles the delay time
+      (( retries-- ))
+    fi
+  done
+
+  echo "Command failed after $max_retries attempts."
+  return 1
+}
